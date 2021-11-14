237f58dd4b80f1a755229b5bbfd18d3d7cd5335b
Emily Wu <emilywu0227@gmail.com>
2021-11-14 14:10:30 -0500

    added users and tweeter routes and tested

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+// import users from "./routes/api/users";
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -9,7 +13,11 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users), app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
