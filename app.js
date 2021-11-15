098c939a71d3adeb868c82864632555c62129397
Emily Wu <emilywu0227@gmail.com>
2021-11-14 23:19:24 -0500

    backend tweets work and tested

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
 // import users from "./routes/api/users";
 
 mongoose
@@ -12,12 +14,16 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
-app.use("/api/users", users), app.use("/api/tweets", tweets);
-
-const port = process.env.PORT || 3000;
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//need to place these before the routes
+
+// app.get("/", (req, res) => res.send("Hello World"));
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use("/api/users", users), app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
